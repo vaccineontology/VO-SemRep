b41e2f21189c4b23b13fcc9c0e3c8d94585b64bf
virtualdann <imdan@umich.edu>
2024-02-05 01:14:23 -0500

    Create config.py

---
+++ Applications/config.py
@@ -0,0 +1,8 @@
+# Registered email with NCIB
+email = "enter@email.here"
+# number of abstracts being fetched at one time
+batch_size = 10
+# range of data (ex: 5 years)
+reldate_days = 365 * 5
+# max result (-1 to fetch all)
+max_res = 5
